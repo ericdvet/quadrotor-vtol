449d202f6685b762caa520d6c06db849eb83ffe0
ericdvet <ericdvet@gmail.com>
2025-03-11 21:14:36 -0700

    trajectory controller seems fine

--- TestHarnesses/testPositionControl.py
+++ TestHarnesses/testPositionControl.py
@@ -13,60 +13,38 @@ t = 0
 x = np.array([0, 0, -0.046, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 u = np.array([4000, 4000, 4000, 4000])
 
+waypoint = np.array([0, 0, -1000])
+
 quad = QuadrotorPhysicalConstants.quad
 Quadrotor = QuadrotorModel.QuadrotorModel(quad, dt)
 
-traj = QuadrotorController.Trajectory(x, np.array([0, 0, 1]))
-env = QuadrotorController.Environment(x, u)
-PC = QuadrotorController.PositionController(dT = dt)
+MV = QuadrotorController.ManeuverController(dT = dt)
 
+thrust_hist = []
+distance_hist = []
 t_hist = []
-x_hist = []
-u_hist = []
 
 for i in range(200):
-    t_hist.append(t)
-    x_hist.append(x)
-    u_hist.append(u)
 
     x = Quadrotor.update(t, x, u)
-    traj = QuadrotorController.Trajectory(x, np.array([0, 0, 1]))
-    env = QuadrotorController.Environment(x, u)
-    pos, vel, roll, pitch, yaw = PC.control(env, traj, t)
+
+    m = QuadrotorController.Environment(x, u)
+    traj = QuadrotorController.Trajectory(x, waypoint)
+
+    motorPitch, motorRoll, motorYaw, motorThrust, CMD_w = MV.control(m, traj)
+
+    thrust_hist.append(motorThrust)
+    distance_hist.append(abs(waypoint[2] - x[2]))
+    t_hist.append(t)
+
     t += dt
 
-    print("Commands: ", roll, pitch, yaw)
-
-x_hist = np.array(x_hist)
-# u_hist = np.array(u_hist)
-
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.plot(x_hist[:, 0], x_hist[:, 1], abs(x_hist[:, 2]))
-# ax.set_title('Position')
-# ax.set_xlabel('North [m]')
-# ax.set_ylabel('East [m]')
-# ax.set_zlabel('Down [m]')
-# ax.set_xlim(-1, 1)
-# ax.set_ylim(-1, 1)
-# plt.grid()
-
-# plt.figure()
-# plt.plot(t_hist, x_hist[:, 3:6])
-# plt.title('Attitude')
-# plt.legend(['Yaw', 'Pitch', 'Roll'])
-# plt.xlabel('Time [s]')
-# plt.ylabel('Angle [rad]')
-# plt.grid()
-
-# plt.figure()
-# plt.title("Position over time")
-# plt.plot(t_hist, x_hist[:, 0], label="North")
-# plt.plot(t_hist, x_hist[:, 1], label="East")
-# plt.plot(t_hist, x_hist[:, 2], label="Down")
-# plt.legend()
-# plt.xlabel("Time [s]")
-# plt.ylabel("Position [m]")
-# plt.grid()
-
-# plt.show()
+plt.figure()
+plt.plot(t_hist, thrust_hist / np.linalg.norm(thrust_hist))
+plt.plot(t_hist, distance_hist / np.linalg.norm(distance_hist))
+plt.title("Thrust vs Distance Away from Object")
+plt.xlabel("Distance Away from Object [m]")
+plt.ylabel("Thrust [N]")
+plt.legend(["Thrust", "Distance"])
+plt.grid()
+plt.show()
